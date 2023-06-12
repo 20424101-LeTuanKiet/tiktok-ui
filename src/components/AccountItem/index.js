c46af3bb3e284847f44af0b7fcceca22e190d334
20424101-LeTuanKiet <20424101@student.hcmus.edu.vn>
2023-06-12 12:46:06 +0700

    Build UI for Header v2

---
+++ src/components/AccountItem/index.js
@@ -0,0 +1,28 @@
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import classNames from 'classnames/bind';
+import styles from './AccountItem.module.scss';
+
+const cx = classNames.bind(styles);
+
+function AccountItem() {
+    return (
+        <div className={cx('wrapper')}>
+            <img
+                className={cx('avatar')}
+                src="https://p16-sign-va.tiktokcdn.com/tos-maliva-avt-0068/0094032b899e8d5a889f33adb3a828af~c5_300x300.webp?x-expires=1686718800&x-signature=OxdR9%2FJYnHJVizosn7856wRE%2BDg%3D"
+                alt="hoaa"
+            />
+            <div className={cx('info')}>
+                <h4 className={cx('name')}>
+                    <span>Nguyễn Văn A</span>
+                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+                </h4>
+                <span className={cx('username')}>nguyenvana</span>
+            </div>
+        </div>
+    );
+}
+
+export default AccountItem;
